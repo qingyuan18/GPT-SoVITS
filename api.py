429abe90381ec8c7def0f6c7f62211c5fcd093ba
tangqy <121102723@qq.com>
2024-05-17 09:36:23 +0800

    debug on sagemaker server

--- api.py
+++ api.py
@@ -712,7 +712,7 @@ async def invocations(request: Request):
     print(f"invocations {json_post_raw=}")
     opt=parse_obj_as(InferenceOpt,json_post_raw)
     print(f"invocations {opt=}")
-    return get_tts_wav(opt.refer_wav_path, opt.prompt_text, opt.prompt_language, opt.text, opt.text_language, opt.cut_punc, opt.output_s3uri)
+    return get_tts_wav(opt.refer_wav_path, opt.prompt_text, opt.prompt_language, opt.text, opt.text_language, opt.output_s3uri)
     
 
 @app.post("/set_model")
