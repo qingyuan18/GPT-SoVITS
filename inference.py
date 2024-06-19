2e60c3ed2d759b237b294a92cbe68a5880bcc22a
tangqy <121102723@qq.com>
2024-06-19 12:39:07 +0800

    update stream sagemaker inference to use wav format

--- inference.py
+++ inference.py
@@ -36,7 +36,8 @@ def write_wav_to_s3(audio_bytes_values,output_s3uri=""):
     
     bucket, key = get_bucket_and_key(output_s3uri)
     key = f'{key}gpt_sovits_{int(time.time())}.wav'
-    file_obj = io.BytesIO(audio_bytes_values)
+    #file_obj = io.BytesIO(audio_bytes_values)
+    file_obj = audio_bytes_values
     s3_client.upload_fileobj(
             Fileobj=file_obj,
             Bucket=bucket,
