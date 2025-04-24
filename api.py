a4d9ec7162403fa2e1800721546bf12b48c08967
tangqy <121102723@qq.com>
2025-04-25 06:45:39 +0800

    open v4 stream mode

--- api.py
+++ api.py
@@ -1087,7 +1087,7 @@ parser.add_argument(
 )
 # bool值的用法为 `python ./api.py -fp ...`
 # 此时 full_precision==True, half_precision==False
-parser.add_argument("-sm", "--stream_mode", type=str, default="close", help="流式返回模式, close / normal / keepalive")
+parser.add_argument("-sm", "--stream_mode", type=str, default="normal", help="流式返回模式, close / normal / keepalive")
 parser.add_argument("-mt", "--media_type", type=str, default="wav", help="音频编码格式, wav / ogg / aac")
 parser.add_argument("-st", "--sub_type", type=str, default="int16", help="音频数据类型, int16 / int32")
 parser.add_argument("-cp", "--cut_punc", type=str, default="", help="文本切分符号设定, 符号范围,.;?!、，。？！；：…")
